0d836239a660eda4920a114ed0f96a0e324efd34
molakd.02 <molakd.02@B121-13>
2021-05-26 11:29:46 +0200

    zmena2

--- pr01/Form1.Designer.cs
+++ pr01/Form1.Designer.cs
@@ -51,6 +51,7 @@
             this.button2.TabIndex = 1;
             this.button2.Text = "Dobrý den pane Molák";
             this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
             // 
             // Form1
             // 
