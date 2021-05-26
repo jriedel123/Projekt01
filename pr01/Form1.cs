49c0095cd9b9fb5293d6547e52d9b01f4c1df4c0
hubejsosol <losikt.03@spst.eu>
2021-05-26 11:24:07 +0200

    tlac2

--- pr01/Form1.cs
+++ pr01/Form1.cs
@@ -21,5 +21,10 @@ namespace pr01
         {
             MessageBox.Show("Ahoj lidi!");
         }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("cahoj");
+        }
     }
 }
